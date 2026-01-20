d77a851b6eebae490b642fa0bf42409bd7681bec
hcxerry <montaboncherry238@gmail.com>
2026-01-20 15:28:04 +0800

    Add Customer.java file to the project

---
+++ Laboratory Activity#3-4/Customer.java
@@ -0,0 +1,87 @@
+public class Customer {
+
+    
+    private String name;
+    private int points;
+
+    
+    private static int totalCustomers = 0;
+
+    
+    public Customer() {
+        this.name = "Unknown";
+        
+        this.points = 0;
+        
+        totalCustomers++;
+    }
+    public Customer(String name, int points) {
+        
+        this.name = name;
+        
+        this.points = points;
+        
+        totalCustomers++;
+    }
+
+    
+    public Customer(String name) {
+        this.name = name;
+        
+        this.points = 0;
+        
+        totalCustomers++;
+    }
+
+    
+    public void addPoints(int earnedPoints) {
+        
+        this.points += earnedPoints;
+    }
+
+    public void redeemReward() {
+        
+        if (points >= 100) {
+            
+            System.out.println("Congratulations " + name + "! You redeemed a free drink!");
+            
+            points -= 100;
+            
+            System.out.println("Remaining Points: " + points);
+        } else {
+            System.out.println(name + " does not have enough points to redeem a drink.");
+            
+            System.out.println("Current Points: " + points);
+        }
+    }
+
+    public void displayCustomerInfo() {
+        System.out.println("Customer Name: " + name);
+        
+        System.out.println("Points: " + points);
+        
+        System.out.println("---------------------------");
+    }
+
+    public int getPoints() {
+     
+        return points;
+    }
+    public static void displayTotalCustomers() {
+        
+        System.out.println("The Total Registered Customers: " + totalCustomers);
+    }
+
+    public static void getAllowedRewardsRedemption(Customer customer) {
+        
+        int redeemable = customer.points / 100;
+        
+        System.out.println(customer.name + " can now redeem " + redeemable + " free drink(s).");
+    }
+}
+
+
+// Cherry R. Montabon 
+// AI THAT USE ChatGPT-5 
+// Conversation of AI 
+// https://chatgpt.com/share/68f4ad01-f3d0-8005-b1e7-7799dd423efe
